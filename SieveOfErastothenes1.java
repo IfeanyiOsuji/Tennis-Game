c561fcc9dac5c5f05eb89c82737ab608b86bf980
IfeanyiOsuji <oindubuisi@gmail.com>
2020-12-09 11:19:44 +0100

    Determining prime numbers in a consecutive list

--- SieveOfErastothenes1.java
+++ SieveOfErastothenes1.java
@@ -1,8 +1,8 @@
 public class SieveOfErastothenes1 {
     // Determining prime numbers in a given consecutive list
     public static boolean [] primeNumbers;
-    public static boolean [] onlyPrime;
 
+    // create a boolean array and initialize all entries to it as true;
     public static boolean [] fillArray(int n){
         primeNumbers = new boolean[n+1];
         for(int a=0; a<n; a++){
@@ -11,10 +11,12 @@ public class SieveOfErastothenes1 {
         return primeNumbers;
 
     }
+    // a valie in the array will eventually be false if it not a prime, else true;
     public static boolean [] checkForNonPrimes(boolean[]n){
         for(int i=2; i*i<n.length;i++ ){
-            if(primeNumbers[i]==true)
-            for(int j=i*i; j <n.length; j+=i){
+            if(primeNumbers[i]==true) // if primeNumbers[i] is not changed, then it is prime
+
+            for(int j=i*i; j <n.length; j+=i){// update all multiples of i;
 
                     primeNumbers[j] = false;
             }
